4e8599f53b1f13e06851b7773eefb36dcc229e74
entr <entr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-02 20:14:18 +0000

    Adding first version of my plugin
    git-svn-id: http://plugins.svn.wordpress.org/gravity-forms-upload-rules/trunk@721747 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/form_editor.js
@@ -0,0 +1,77 @@
+(function($,window,document,undefined) {
+  
+  function gforms_uprules_dims_ruletype_change( type ) {
+    
+    $('#field_uprules_dims_ruletype').val( type );
+    switch( type ) {
+      case 'exact':
+        $('.uprules_dims_fields_exact').show();
+        $('.uprules_dims_fields_conditional').hide();
+        break;
+      case 'conditional':
+        $('.uprules_dims_fields_conditional').show();
+        $('.uprules_dims_fields_exact').hide();
+        break;
+      default:
+        $('.uprules_dims_fields_exact, .uprules_dims_fields_conditional').hide();
+    }
+  }
+
+	$(document).bind('gform_load_field_settings', function(e,field,form) {
+		$('#field_uprules_filesize').val(field.uprules_filesize_limit);
+		$('#field_uprules_filesize_dim').val(field.uprules_filesize_dim);
+    
+    $('#field_uprules_dims_exact_width').val( field.uprules_dims_exact_width );
+    $('#field_uprules_dims_exact_height').val( field.uprules_dims_exact_height );
+    $('#field_uprules_dims_minwidth').val( field.uprules_dims_minwidth );
+    $('#field_uprules_dims_minheight').val( field.uprules_dims_minheight );
+    $('#field_uprules_dims_maxwidth').val( field.uprules_dims_maxwidth );
+    $('#field_uprules_dims_maxheight').val( field.uprules_dims_maxheight );
+    
+    gforms_uprules_dims_ruletype_change( field.uprules_dims_ruletype );
+    
+		/* found a better way to do it
+		if ( $.inArray( field.type, ['post_image','fileupload'] ) > -1 ) {
+			$('.uprules_filesize_setting, .uprules_dimensions_setting').show();
+    }
+		*/
+	});
+  
+  $(function(){
+    
+		//$('#field_uprules_filesize_dim').triggerHandler('change');
+    $('#field_uprules_dims_ruletype').change( function() { gforms_uprules_dims_ruletype_change( $(this).val() ); } );
+  });
+  
+	window.fieldSettings.fileupload += ', .uprules_filesize_setting, .uprules_dimensions_setting';
+	window.fieldSettings.post_image += ', .uprules_filesize_setting, .uprules_dimensions_setting';
+	
+	if ( $.isFunction( window['SetDefaultValues_fileupload'] ) )
+		window['__SetDefaultValues_fileupload'] = window['SetDefaultValues_fileupload'];
+		
+	window['SetDefaultValues_fileupload'] = function(field) {
+		if ( $.isFunction( window['__SetDefaultValues_fileupload'] ) )
+			field = window['__SetDefaultValues_fileupload'](field);
+			
+		field = $.extend( field, {
+			uprules_filesize_dim	:	'kb'
+		} );
+		return field;
+	};
+	
+	if ( $.isFunction( window['SetDefaultValues_post_image'] ) )
+		window['__SetDefaultValues_post_image'] = window['SetDefaultValues_post_image'];
+		
+	window['SetDefaultValues_post_image'] = function(field) {
+		if ( $.isFunction( window['__SetDefaultValues_post_image'] ) )
+			field = window['__SetDefaultValues_post_image'](field);
+			
+		field = $.extend( field, {
+			uprules_filesize_dim	:	'kb'
+		} );
+		
+		return field;
+	};
+	
+	
+})(jQuery,window,document);
